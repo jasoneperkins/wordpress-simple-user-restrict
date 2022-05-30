ff021bea38b5dd021c5b72b8eea25dfbfd0f5efc
jasoneperkins <jasonperkins77@gmail.com>
2022-05-30 13:32:18 -0400

    Add whitelisting of allowed IP addresses

--- functions.php
+++ functions.php
@@ -7,7 +7,12 @@
 // Redirects unauthenticated users to the WordPress login page
 add_action('template_redirect', function() {
 	$loggedIn		= is_user_logged_in();	// True if user is logged in.
-	$authenticated	= $loggedIn;		// True if user is allowed to view folders and files.
+	$whiteList		= array(
+		'xx.xxx.xxx.xxx',
+		'xxxx:xxx:xxxx:xxxx:xxxx:xxxx:xxxx:xxxx',
+	); // Array of IP addresses allowed whether user is logged in or not.
+	$whiteListed	= in_array($_SERVER['REMOTE_ADDR'], $whiteList); // True if user's IP address is in the list of whitelisted addresses.
+	$authenticated	= $loggedIn || $whiteListed; // True if user is allowed to view folders and files (logged in or whitelisted).
 	
 	// Allow AJAX processes.
 	if ( is_admin() || wp_doing_ajax() ) {
@@ -18,5 +23,4 @@ add_action('template_redirect', function() {
 	if ( !$authenticated ) {
 		wp_redirect( esc_url( wp_login_url() ), 307 );
 	}
-
 });
