5f56c544f95b7d34788ade64303ab5bddf931f9f
jasoneperkins <jasonperkins77@gmail.com>
2022-05-30 12:44:16 -0400

    Add exception to allow AJAX processes.

--- functions.php
+++ functions.php
@@ -8,7 +8,13 @@
 add_action('template_redirect', function() {
 	$loggedIn		= is_user_logged_in();	// True if user is logged in.
 	$authenticated	= $loggedIn;		// True if user is allowed to view folders and files.
+	
+	// Allow AJAX processes.
+	if ( is_admin() || wp_doing_ajax() ) {
+		return;
+	}
 
+	// Redirect unauthenticated users to the WordPress login page.
 	if ( !$authenticated ) {
 		wp_redirect( esc_url( wp_login_url() ), 307 );
 	}
